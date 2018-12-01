3f17c3f961feace2d5639e9854c1fca6dfba95f6
Sam Anderson <samel.2710@gmail.com>
2018-12-01 11:04:09 +0000

    Initial answers for 01
    Could be improved by reading the input from URL rather than local file.
    Run functions in parallel.

---
+++ 01/run.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+// Set starting point of frequency counter
+const START_FREQ = 0;
+
+// Initialise counters/flags
+let freq = START_FREQ;
+const seenFreq = new Set();
+let firstSeen = null;
+let index = 0;
+
+// TODO Read this data from the Advent of code URL rather than local
+fs.readFile('./input.txt', 'utf8', (err, contents) => {
+    if(err) throw err;
+    
+    // Convert data to array 
+    const data = contents.split("\n");
+    const dataLength = data.length;
+
+    // Loop through array and increment frequency counter
+    data.forEach(entry => {
+        const change = parseInt(entry, 10);
+        freq += change;
+    });
+    console.log(`Answer 1 = ${freq}`);
+
+    // Reset frequency count;
+    freq = START_FREQ;
+
+    // Build up Set of observed frequencies, exit when duplicate seen
+    while(!firstSeen) {
+        const thisIndex = index % dataLength;
+        const thisEntry = parseInt(data[thisIndex], 10);
+        freq += thisEntry;
+
+        if(seenFreq.has(freq)) {
+            firstSeen = freq;
+        }
+        seenFreq.add(freq);
+        index++;
+    };
+    console.log(`Answer 2 = ${firstSeen}`);
+});
