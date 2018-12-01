f2929dde6a9f2bfb110692458494c5b6e561e14b
Sam Anderson <samel.2710@gmail.com>
2018-12-01 11:53:16 +0000

    Refactor solutions into seperate functions

--- 01/run.js
+++ 01/run.js
@@ -3,42 +3,56 @@ const fs = require('fs');
 // Set starting point of frequency counter
 const START_FREQ = 0;
 
-// Initialise counters/flags
-let freq = START_FREQ;
-const seenFreq = new Set();
-let firstSeen = null;
-let index = 0;
-
 // TODO Read this data from the Advent of code URL rather than local
 // https://adventofcode.com/2018/day/1/input
 fs.readFile('./input.txt', 'utf8', (err, contents) => {
     if(err) throw err;
     
-    // Convert data to array 
-    const data = contents.split("\n");
-    const dataLength = data.length;
+    // Convert data to array and convert strings to ints
+    const data = contents.split("\n").map(s => parseInt(s, 10));
 
-    // Loop through array and increment frequency counter
-    data.forEach(entry => {
-        const change = parseInt(entry, 10);
-        freq += change;
-    });
-    console.log(`Answer 1 = ${freq}`);
+    // Question 1
+    const answer1 = applyChangeArray(START_FREQ, data);
+    console.log(`Answer 1 = ${answer1}`);
 
-    // Reset frequency count;
-    freq = START_FREQ;
-
-    // Build up Set of observed frequencies, exit when duplicate seen
-    while(!firstSeen) {
-        const thisIndex = index % dataLength;
-        const thisEntry = parseInt(data[thisIndex], 10);
-        freq += thisEntry;
-
-        if(seenFreq.has(freq)) {
-            firstSeen = freq;
-        }
-        seenFreq.add(freq);
-        index++;
-    };
-    console.log(`Answer 2 = ${firstSeen}`);
+    // Question 2
+    const answer2 = findDuplicateResult(START_FREQ, data);
+    console.log(`Answer 2 = ${answer2}`);
 });
+
+/**
+ * Reduce array of changes to a signle accumulated value
+ * @param {number} start - Start value for result
+ * @param {array} arr - Array of change values to apply
+ */
+const applyChangeArray = (start, arr) => {
+    return arr.reduce((acc, current) => (acc + current), start);
+}
+
+/**
+ * Loop through change array continuously, building a Set of
+ * unique results and exiting when the first duplicate is seen
+ * @param {number} start - Start value for result
+ * @param {array} arr - Array of change values to apply
+ */
+const findDuplicateResult = (start, arr) => {
+    let i = 0;
+    let count = start;
+    let firstDup = null;
+    const results = new Set();
+    const length = arr.length;
+
+    // While no result found
+    while(!firstDup) {
+        // Calculate intermediate result
+        count += arr[i % length]; 
+        // If result has been seen before then register as result
+        if(results.has(count)) {
+            firstDup = count;
+        }
+        // Add intermediate to result Set
+        results.add(count);
+        i++;
+    }
+    return firstDup;
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # advent-of-code-2018
+My solutions to the 2018 advent of code in Node.js
+
+1. Chronal Calibration
+
+   Take an array of values, calculate the final result & detect first duplicates
