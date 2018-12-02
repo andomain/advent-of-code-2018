bcd626108db462b14654c1f3cf0ebb8607b982f9
Sam Anderson <samel.2710@gmail.com>
2018-12-02 20:58:30 +0000

    Add part 2
    Another probable inefficient solution due to the multiple array loops

--- 02/index.js
+++ 02/index.js
@@ -3,13 +3,20 @@ const fs = require('fs');
 fs.readFile('./input.txt', 'utf8', (err, content) => {
     if (err) throw err;
 
+    const lines = content.split('\n');
+
     // Process input string
-    const data = processInput(content);
+    const data = sortAndStrip(lines);
     // Convert input data into lookup of each ID
     const lookup = buildLookup(data);
-    // Comvert array of lookups into checksum
-    const result = calcChecksum(lookup);
-    console.log(`Answer = ${result}`);
+    // Convert array of lookups into checksum
+    const result1 = calcChecksum(lookup);
+    console.log(`Answer 1 = ${result1}`);
+
+    // Search for similar strings
+    const result2 = getSimilar(lines);
+    console.log(`Answer 2 - ${result2}`);
+
 });
 
 /**
@@ -18,12 +25,10 @@ fs.readFile('./input.txt', 'utf8', (err, content) => {
  * non-prepeated characters
  * @param {string} input - Multiline input string
  */
-const processInput = input => {
-    return input.split('\n')
-        .map(l => l.split('').sort()
-            .filter(filterRepeatChar).join('')
-        )
-        .map(str => maxStringLengths(str));
+const sortAndStrip = arr => {
+    return arr.map(l => l.split('').sort()
+        .filter(filterRepeatChar).join('')
+    ).map(str => maxStringLengths(str));
 }
 
 /**
@@ -54,19 +59,19 @@ calcChecksum = lookup => {
     let tmp = 1;
     Object.keys(lookup).forEach(key => (tmp *= lookup[key]));
     return tmp;
-} 
+}
 
 /**
  * Build a Set of unique repeated character lengths
  * @param {string} - Input string 
  */
 maxStringLengths = str => {
-    const n = str.length; 
-    const res = new Set(); 
-    let count = 1; 
+    const n = str.length;
+    const res = new Set();
+    let count = 1;
 
-    for(let i = 0; i < n; i++) {
-        if((i < (n - 1)) && str[i] === str[i+1]) {
+    for (let i = 0; i < n; i++) {
+        if ((i < (n - 1)) && str[i] === str[i + 1]) {
             count++;
         } else {
             res.add(count);
@@ -84,4 +89,58 @@ filterRepeatChar = (ch, idx, arr) => {
     if (idx === 0) return ch === arr[idx + 1];
     if (idx === arr.length - 1) return ch === arr[idx - 1];
     return ch === arr[idx + 1] || ch === arr[idx - 1];
+}
+
+/**
+ * Loop through array of lines and compare for similarity
+ * @param {array} - arr - Array of lines
+ */
+getSimilar = arr => {
+    let a, b;
+    const length = arr.length;
+    let result = false;
+
+    for (let i = 0; i <= length - 2; i++) {
+        a = arr[i];
+        for (let j = 1; j <= length - 1; j++) {
+            if (i !== j) {
+                b = arr[j];
+                result = compareString(a, b);
+                if (result) break;
+            }
+        }
+        if(result) break;
+    }
+    return result;
+}
+
+/**
+ * Compare two strings to check for similarity.
+ * If only different by <=n chars then return
+ * matching characters
+ * @param {string} a
+ * @param {string} b
+ * @param {number} similarity - Max different chars
+ */
+compareString = (a, b, similarity = 1) => {
+    const aSplit = a.split('');
+    const bSplit = b.split('');
+    const length = aSplit.length;
+
+    let different = 0;
+    let result = '';
+
+    for (let i = 0; i <= length - 1; i++) {
+        if (aSplit[i] === bSplit[i]) {
+            result += aSplit[i];
+        } else {
+            different += 1;
+            if (different > similarity) {
+                different = 0;
+                return false;
+            }
+        }
+    }
+    return result;
+
 }
