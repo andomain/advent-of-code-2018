d89d3bc96a7c0b14acb0292f2e668333e5585f9c
Sam <sam.anderson@tappit.com>
2019-12-04 18:34:30 +0000

    Add part 2
    Refactored to reuse shared validated results

--- 2019/04/index.ts
+++ 2019/04/index.ts
@@ -1,10 +1,8 @@
 import { Validator } from '../utils/Validator';
 import { printResult, readFile } from '../utils';
 
-const validation = (candidate: number): boolean => {
+const validateDescending = (candidate: number): boolean => {
   const digits = candidate.toString().split('').map(d => Number(d));
-  let double = false;
-
   for (let i = 0; i < digits.length - 1; i += 1) {
     const current = digits[i];
     const next = digits[i + 1];
@@ -12,22 +10,68 @@ const validation = (candidate: number): boolean => {
     if (next < current) {
       return false;
     }
-    if (!double && current === next) {
-      double = true;
+  }
+  return true;
+}
+
+const validateDouble = (candidate: number): boolean => {
+  const digits = candidate.toString().split('').map(d => Number(d));
+
+  for (let i = 0; i < digits.length; i += 1) {
+    const current = digits[i];
+    const next = digits[i + 1];
+
+    if (current === next) {
+      return true
     }
   }
-  return double;
+  return false;
+}
+
+const validateStrictDouble = (candidate: number): boolean => {
+  const digits = candidate.toString().split('').map(d => Number(d));
+
+  for (let i = 0; i < digits.length; i += 1) {
+    const current = digits[i];
+    let count = 1;
+
+    for (let j = i + 1; j < digits.length; j += 1) {
+      if (current === digits[j]) {
+        count += 1;
+        i = j;
+      } else {
+        if (count === 2) {
+          return true;
+        } else {
+          j = digits.length;
+        }
+      }
+    }
+    if (count == 2) {
+      return true;
+    }
+  }
+  return false;
 }
 
 const input = readFile(`${__dirname}/input.txt`);
 const [lower, upper] = input.split('-').map(n => Number(n));
 
-const passwordValidator = new Validator([validation]);
+const descendingValidator = new Validator<number>([validateDescending]);
+const doubleValidator = new Validator<number>([validateDouble]);
+const strictDoubleValidator = new Validator<number>([validateStrictDouble]);
 
 // Dumb implementation: could be optimised
-let count = 0
+const nonDescending = [];
+
+// Get all that pass the non descending rule
 for (let i = lower; i <= upper; i += 1) {
-  count += passwordValidator.validate(i) ? 1 : 0;
+  if (descendingValidator.validate(i)) {
+    nonDescending.push(i);
+  }
 }
 
-printResult(4, count);
+const valid1 = nonDescending.filter(n => doubleValidator.validate(n));
+const valid2 = valid1.filter(n => strictDoubleValidator.validate(n))
+
+printResult(4, valid1.length, valid2.length);
