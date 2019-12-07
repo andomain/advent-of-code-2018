8462ce49ab658b44489e5f123c9bc1bf3a598e1c
Sam Anderson <sam@andomain.co.uk>
2019-12-07 14:00:48 +0000

    Add consitional jump to Intocde for day 5 pt 2

--- 2019/05/index.ts
+++ 2019/05/index.ts
@@ -2,16 +2,24 @@ import { readFile, printResult } from '../utils';
 import { Intcode } from '../utils/Intcode';
 
 // Get data
-const init = readFile(`${__dirname}/input.txt`).split(',').map(n => Number(n));
+const init = readFile(`${__dirname}/input.txt`)
+    .split(',')
+    .map(n => Number(n));
 
 const intCode = new Intcode(init);
 intCode.process(1);
 const result1 = intCode.output;
+intCode.reset();
 
 if (result1 === null) {
-  throw new Error('No output set');
+    throw new Error('No output set');
 }
 
-printResult(5, result1);
+intCode.process(5);
+const result2 = intCode.output;
 
+if (result2 === null) {
+    throw new Error('No output set');
+}
 
+printResult(5, result1, result2);
--- 2019/utils/Intcode.ts
+++ 2019/utils/Intcode.ts
@@ -1,97 +1,119 @@
 interface IIntcode {
-  input: number[];
-  output: number | null;
+    input: number[];
+    output: number | null;
 }
 
-const ADD_OPCODE = 1;
-const MULT_OPCODE = 2;
-const INPUT_OPCODE = 3;
-const OUTPUT_OPCODE = 4;
-const FINISH_OPCODE = 99;
+const ADD = 1;
+const MULT = 2;
+const INPUT = 3;
+const OUTPUT = 4;
+const JUMP_TRUE = 5;
+const JUMP_FALSE = 6;
+const LESS_THAN = 7;
+const EQUALS = 8;
+const FINISH = 99;
 
 export class Intcode implements IIntcode {
-  public init: number[];
-  private processed: number[];
-  public output: number | null = null;
+    public init: number[];
+    private processed: number[];
+    public output: number | null = null;
 
-  constructor(public input: number[]) {
-    this.init = Array.from(input);
-    this.processed = Array.from(input);
-  }
-
-  set(position: number, value: number, mode: number = 1): void {
-    if (mode === 1) {
-      this.processed[position] = value;
-    } else {
-      this.processed[this.processed[position]] = value;
-    }
-  }
-
-  get(position: number, mode: number = 1): number {
-    if (mode === 1) {
-      return this.processed[position];
+    constructor(public input: number[]) {
+        this.init = Array.from(input);
+        this.processed = Array.from(input);
     }
 
-    return this.processed[this.processed[position]];
-  }
-
-  reset(): void {
-    this.processed = Array.from(this.init);
-  }
-
-  private readOpcode = (rawCode: number): number[] => {
-    const inter = rawCode
-      .toString()
-      .padStart(5, '0')
-      .match(/(\d)(\d)(\d)(\d\d)/);
-
-    if (inter !== null) {
-      return inter.slice(1).map(s => Number(s)).reverse();
+    set(position: number, value: number, mode: number = 1): void {
+        if (mode === 1) {
+            this.processed[position] = value;
+        } else {
+            this.processed[this.processed[position]] = value;
+        }
     }
-    throw new Error("Couldn't generate parameters");
-  }
 
-  process(input?: number): void {
-    let position = 0;
+    get(position: number, mode: number = 1): number {
+        if (mode === 1) {
+            return this.processed[position];
+        }
 
-    while (this.processed[position] !== FINISH_OPCODE) {
-      const op = this.readOpcode(this.processed[position]);
-      const [opCode, mode1, mode2, mode3] = op;
-
-      const opA = this.get(position + 1, mode1);
-      const opB = this.get(position + 2, mode2);
-      // const opC = this.get(position + 3, mode3);
-      const destAddr = this.processed[position + 3];
-
-      switch (opCode) {
-        case ADD_OPCODE:
-          this.processed[destAddr] = this.add(opA, opB);
-          position += 4;
-          break;
-        case MULT_OPCODE:
-          this.processed[destAddr] = this.multiply(opA, opB);
-          position += 4;
-          break;
-        case INPUT_OPCODE:
-          if (!input) throw new Error('Please supply an input');
-          this.processed[destAddr] = input;
-          position += 2;
-          break;
-        case OUTPUT_OPCODE:
-          this.output = opA;
-          position += 2;
-          break;
-
-        default: throw new Error(`Unknown opcode ${opCode}`);
-      }
+        return this.processed[this.processed[position]];
     }
-  }
 
-  private add(a: number, b: number): number {
-    return a + b;
-  }
+    reset(): void {
+        this.processed = Array.from(this.init);
+    }
 
-  private multiply(a: number, b: number): number {
-    return a * b;
-  }
+    private readOpcode = (rawCode: number): number[] => {
+        const inter = rawCode
+            .toString()
+            .padStart(5, '0')
+            .match(/(\d)(\d)(\d)(\d\d)/);
+
+        if (inter !== null) {
+            return inter
+                .slice(1)
+                .map(s => Number(s))
+                .reverse();
+        }
+        throw new Error("Couldn't generate parameters");
+    };
+
+    process(input?: number): void {
+        let position = 0;
+
+        while (this.processed[position] !== FINISH) {
+            const op = this.readOpcode(this.processed[position]);
+            const [opCode, mode1, mode2, mode3] = op;
+
+            const opA = this.get(position + 1, mode1);
+            const opB = this.get(position + 2, mode2);
+            const opC = this.get(position + 3, mode3);
+            const destAddr = this.processed[position + 3];
+
+            switch (opCode) {
+                case ADD:
+                    this.processed[destAddr] = this.add(opA, opB);
+                    position += 4;
+                    break;
+                case MULT:
+                    this.processed[destAddr] = this.multiply(opA, opB);
+                    position += 4;
+                    break;
+                case INPUT:
+                    if (!input) throw new Error('Please supply an input');
+                    this.processed[destAddr] = input;
+                    position += 2;
+                    break;
+                case JUMP_TRUE:
+                    position = opA !== 0 ? opB : position + 3;
+                    break;
+                case JUMP_FALSE:
+                    position = opA === 0 ? opB : position + 3;
+                    break;
+                case LESS_THAN:
+                    this.processed[destAddr] = opA < opB ? 1 : 0;
+                    position += 4;
+                    break;
+                case EQUALS:
+                    this.processed[destAddr] = opA === opB ? 1 : 0;
+                    position += 4;
+                    break;
+                case OUTPUT:
+                    this.output = opA;
+                    position += 2;
+                    break;
+
+                default:
+                    throw new Error(`Unknown opcode ${opCode}`);
+            }
+        }
+    }
+
+    private add(a: number, b: number): number {
+        return a + b;
+    }
+
+    private multiply(a: number, b: number): number {
+        return a * b;
+    }
 }
