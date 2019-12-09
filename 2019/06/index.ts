93697c1153e254e046bfd4e398d63f6f1a74a8b8
Sam <sam.anderson@tappit.com>
2019-12-09 13:23:21 +0000

    Refactor day 6 part 1 to track planet parent

--- 2019/06/index.ts
+++ 2019/06/index.ts
@@ -1,22 +1,55 @@
 import { readFileLines, printResult } from '../utils';
 
-const input = readFileLines(`${__dirname}/input.txt`).map(orbitData => {
-    const [center, orbit] = orbitData.split(')');
-    return {
-        center,
-        orbit,
-    };
-});
+class Planet {
+  public parent: Planet | null;
+  public children: Planet[];
 
-const orbits = input.reduce((lookup, orbitData) => {
-    const currentCenter = lookup.get(orbitData.center) || [];
-    currentCenter.push(orbitData.orbit);
-    lookup.set(orbitData.center, currentCenter);
-    if (!lookup.has(orbitData.orbit)) {
-        lookup.set(orbitData.orbit, []);
+  constructor(public name: string) {
+    this.parent = null;
+    this.children = [];
+  }
+
+  addChild(child: Planet) {
+    this.children.push(child);
+  }
+}
+
+const planetLookup = readFileLines(`${__dirname}/input.txt`)
+  .reduce((lookup, orbitData) => {
+    const [center, orbit] = orbitData.split(')');
+    let parent = lookup.get(center);
+    let thisPlanet = lookup.get(orbit);
+
+    if (!parent) {
+      parent = new Planet(center);
     }
+    if (!thisPlanet) {
+      thisPlanet = new Planet(orbit);
+    }
+
+    thisPlanet.parent = parent;
+    parent.addChild(thisPlanet);
+
+    lookup.set(orbit, thisPlanet);
+    lookup.set(center, parent);
+
     return lookup;
-}, new Map<string, string[]>());
+  }, new Map<string, Planet>());
+
+const sumOrbits = (planets: Map<string, Planet>, start: string, count = 0) => {
+  const current = planets.get(start);
+  if (!current) {
+    throw new Error(`Unknown planet ${start}`);
+  }
+  if (current.children.length < 1) {
+    return count;
+  }
+  let currentLength = count;
+  current.children.forEach(child => {
+    currentLength += sumOrbits(planets, child.name, count + 1);
+  });
+  return currentLength;
+}
 
 const countOrbits = (
     startPlanet: string,
@@ -32,8 +65,7 @@ const countOrbits = (
         count += countOrbits(orbit, orbits, pathLength + 1);
     });
 
-    return count;
-};
+const result1 = sumOrbits(planetLookup, 'COM');
 
 const result1 = countOrbits('COM', orbits);
 
