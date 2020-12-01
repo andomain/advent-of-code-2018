837386e6dfd89819519dbfdd9431b14bd16fff02
Sam Anderson <sam@andomain.co.uk>
2020-12-01 10:21:21 +0000

    2020 01 Part 2

--- 2020/01/index.ts
+++ 2020/01/index.ts
@@ -20,13 +20,34 @@ const getTwoArrayElementsThatSumTo = (inputArray: number[], target: number): [nu
 
   throw new Error(`Could not find elements summing to ${target}`);
 }
+
+const getThreeArrayElementsThatSumTo = (inputArray: number[], target: number): [number, number, number] => {
+  let startIndex = 0;
+  let maxIndex = inputArray.length - 1;
+
+  while (startIndex <= maxIndex) {
+    try {
+      const tmp = inputArray[startIndex];
+      const [a, b] = getTwoArrayElementsThatSumTo(inputArray.slice(startIndex + 1), TARGET_SUM - tmp);
+      if (tmp + a + b === target) {
+        return [tmp, a, b];
+      } else {
+        startIndex++;
+      }
+    } catch (err) {
+      startIndex++;
+    }
+  }
+  throw new Error(`Could not find 3 elements summing to ${target}`);
 }
+
 const lines: number[] = readFileLines(`${__dirname}/input.txt`)
   .map(Number)
   .sort((a, b) => (a - b));
 
 const [a, b] = getTwoArrayElementsThatSumTo(lines, TARGET_SUM);
 
-const [a, b] = getArrayElementsThatSumTo(lines, TARGET_SUM);
+const [c, d, e] = getThreeArrayElementsThatSumTo(lines, TARGET_SUM);
 
 console.log(`Part 1: ${a * b}`);
+console.log(`Part 2: ${c * d * e}`);
