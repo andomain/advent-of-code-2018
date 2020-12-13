e19fe8069ec49753f6af0f76a5d1804f2fae1a68
Sam Anderson <sam@andomain.co.uk>
2020-12-13 21:56:07 +0000

    2020 Day 13 Part 2 & refactor

--- 2020/13/13.spec.ts
+++ 2020/13/13.spec.ts
@@ -1,10 +1,17 @@
-import { getNextBus, getNextDepartTime, getParameters } from "."
+import { departsAtTime, findTimestamp, getNextBus, getNextDepartTime, getParameters } from "."
 
 describe('getParameters', () => {
   it('reads input', () => {
     const results = getParameters(`${__dirname}/test.txt`);
     expect(results.earliest).toBe(939);
-    expect(results.running).toEqual([7, 13, 59, 31, 19]);
+    // expect(results.running).toEqual([7, 13, 59, 31, 19]);
+    expect(results.routes).toEqual([
+      { id: 7, offset: 0 },
+      { id: 13, offset: 1 },
+      { id: 59, offset: 4 },
+      { id: 31, offset: 6 },
+      { id: 19, offset: 7 },
+    ]);
   });
 });
 
@@ -19,6 +26,25 @@ describe('getNextDepartTime', () => {
 
 describe('getNextBus', () => {
   it('gets the next bus departing after a time', () => {
-    expect(getNextBus(939, [7, 13, 59, 31, 19])).toEqual({ id: 59, time: 944 });
+    const testParams = getParameters(`${__dirname}/test.txt`);
+
+    expect(getNextBus(testParams.earliest, testParams.routes)).toEqual({ id: 59, time: 944 });
+  });
+});
+
+describe('departsAtTime', () => {
+  it('returns true if a bus departs at that time', () => {
+    expect(departsAtTime(15, 5)).toBeTruthy();
+  });
+
+  it('returns false if a bus does not departs at that time', () => {
+    expect(departsAtTime(7, 5)).toBeFalsy();
+  });
+});
+
+describe('findTimestamp', () => {
+  it('finds the time when all buses depart at correct offsets', () => {
+    const testParams = getParameters(`${__dirname}/test.txt`);
+    expect(findTimestamp(testParams.routes)).toBe(1068781);
   });
 });
--- 2020/13/index.ts
+++ 2020/13/index.ts
@@ -1,11 +1,23 @@
 import { readFileLines } from "../utils"
 
-export const getParameters = (filename: string) => {
+type Route = { id: number, offset: number };
+
+export const getParameters = (filename: string): {
+  earliest: number,
+  routes: Route[]
+} => {
   const lines = readFileLines(filename);
+
+  const busRoutes = lines[1].split(',');
 
   return {
     earliest: Number(lines[0]),
-    running: lines[1].split(',').filter(id => id !== 'x').map(Number),
+    routes: busRoutes.reduce((routes: Route[], routeId, offset) => {
+      if (routeId !== 'x') {
+        routes.push({ id: Number(routeId), offset });
+      }
+      return routes;
+    }, []),
   }
 };
 
@@ -19,19 +31,43 @@ export const getNextDepartTime = (after: number, busId: number): number => {
   return Math.ceil(loops) * busId;
 }
 
-export const getNextBus = (departAfter: number, buses: number[]): { id: number, time: number } => buses.reduce((nextBus: { id: number, time: number }, id) => {
-  const time = getNextDepartTime(departAfter, id);
+export const getNextBus = (departAfter: number, buses: Route[]): { id: number, time: number } => buses.reduce((nextBus: { id: number, time: number }, route) => {
+  const time = getNextDepartTime(departAfter, route.id);
 
   if (time < nextBus.time) {
-    nextBus = { id, time }
+    nextBus = { id: route.id, time }
   }
 
   return nextBus;
-}, { id: 0, time: Number.MAX_SAFE_INTEGER })
+}, { id: 0, time: Number.MAX_SAFE_INTEGER });
 
-const { earliest, running } = getParameters(`${__dirname}/input.txt`);
-const nextBus = getNextBus(earliest, running);
+export const departsAtTime = (time: number, id: number) => time % id === 0;
+
+export const findTimestamp = (schedule: Route[]): number => {
+  const routeCount = schedule.length;
+  // Start incrementing by the interval of the first bus
+  let increment = schedule[0].id;
+  let timestamp = increment;
+
+  // For each bus, find the time when that bus departs at the correct offset
+  for (let index = 1; index < routeCount; index++) {
+    const thisRoute = schedule[index];
+    while (!departsAtTime(timestamp + thisRoute.offset, thisRoute.id)) {
+      timestamp += increment;
+    }
+
+    // We now increment by the amount to maintain interval of the previous buses
+    increment *= thisRoute.id;
+  }
+
+  return timestamp;
+}
+
+const { earliest, routes } = getParameters(`${__dirname}/input.txt`);
+const nextBus = getNextBus(earliest, routes);
 
 const part1 = nextBus.id * (nextBus.time - earliest);
 console.log(`Part One: ${part1}`);
 
+const part2 = findTimestamp(routes);
+console.log(`Part Two: ${part2}`);
